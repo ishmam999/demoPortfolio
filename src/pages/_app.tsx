c6db4195da2ffe6711fee6343c2fc08461469119
ishmam999 <ishmam2050@gmail.com>
2023-04-05 02:16:43 +0900

    Initializing demo portfolio app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '@/styles/globals.css'
+import {Montserrat} from "next/font/google"
+import type { AppProps } from 'next/app'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={`${montserrat.variable} font-mont`}>
+      <Component {...pageProps} />
+    </main>
+  )
+}
